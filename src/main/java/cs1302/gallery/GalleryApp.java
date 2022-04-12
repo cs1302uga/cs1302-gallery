a727d68bac817c7605d8bff70bc1d1e973f06954
Michael Cotterell <mepcotterell@gmail.com>
2022-04-12 10:37:57 -0400

    modified starter structure

--- src/main/java/cs1302/gallery/GalleryApp.java
+++ src/main/java/cs1302/gallery/GalleryApp.java
@@ -27,31 +27,6 @@ public class GalleryApp extends Application {
         .setPrettyPrinting()                          // enable nice output when printing
         .create();                                    // builds and returns a Gson object
 
-    /**
-     * Represents a response from the iTunes Search API. This is used by Gson to
-     * create an object from the JSON response body. This class is provided with
-     * project's starter code, and the instance variables are intentionally set
-     * to package private visibility.
-     */
-    private static class ItunesResponse {
-        int resultCount;
-        ItunesResult[] results;
-    } // ItunesResponse
-
-    /**
-     * Represents a result in a response from the iTunes Search API. This is
-     * used by Gson to create an object from the JSON response body. This class
-     * is provided with project's starter code, and the instance variables are
-     * intentionally set to package private visibility.
-     * @see <a href="https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/UnderstandingSearchResults.html#//apple_ref/doc/uid/TP40017632-CH8-SW1">Understanding Search Results</a>
-     */
-    private static class ItunesResult {
-        String wrapperType;
-        String kind;
-        String artworkUrl100;
-        // the rest of the result is intentionally omitted since we don't use it
-    } // ItunesResult
-
     /** {@inheritDoc} */
     @Override
     public void start(Stage stage) {
---
+++ src/main/java/cs1302/gallery/ItunesResponse.java
@@ -0,0 +1,10 @@
+/**
+ * Represents a response from the iTunes Search API. This is used by Gson to
+ * create an object from the JSON response body. This class is provided with
+ * project's starter code, and the instance variables are intentionally set
+ * to package private visibility.
+ */
+public class ItunesResponse {
+    int resultCount;
+    ItunesResult[] results;
+} // ItunesResponse
